7de1efb92197116300810aed6618aeb7aa3c58a9
Kim Tore Jensen <kimtjen@gmail.com>
2020-08-24 13:31:18 +0200

    register jwker type with controller

--- cmd/kafkarator/main.go
+++ cmd/kafkarator/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/nais/kafkarator/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
+	// +kubebuilder:scaffold:imports
 )
 
 var scheme = runtime.NewScheme()
@@ -15,4 +18,11 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	err = kafka_nais_io_v1.AddToScheme(scheme)
+	if err != nil {
+		panic(err)
+	}
+
+	// +kubebuilder:scaffold:scheme
 }
