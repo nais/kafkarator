b4b78b3b1e7177d75be11ce70946f94fa12eed9a
Kim Tore Jensen <kimtjen@gmail.com>
2020-10-08 16:06:19 +0200

    split topic controller logic to avoid kubernetes writes in inner function

--- controllers/topic_controller.go
+++ controllers/topic_controller.go
@@ -14,6 +14,7 @@ import (
 	"github.com/nais/kafkarator/pkg/metrics"
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -31,12 +32,11 @@ const (
 	maxSecretNameLength    = 63
 )
 
-type transaction struct {
-	ctx        context.Context
-	req        ctrl.Request
-	topic      kafka_nais_io_v1.Topic
-	aivenTopic *aiven.KafkaTopic
-	logger     *log.Entry
+type ReconcileResult struct {
+	Requeue bool
+	Status  kafka_nais_io_v1.TopicStatus
+	Secrets []v1.Secret
+	Error   error
 }
 
 type TopicReconciler struct {
@@ -60,10 +60,121 @@ func (r *TopicReconciler) projectWhitelisted(project string) bool {
 	return false
 }
 
+// Write changes to Aiven and return a topic processing status and a list of secrets to send to clusters
+func (r *TopicReconciler) Process(topicResource kafka_nais_io_v1.Topic, logger *log.Entry) ReconcileResult {
+	var err error
+	var hash string
+	var status kafka_nais_io_v1.TopicStatus
+
+	if topicResource.Status != nil {
+		status = *topicResource.Status
+	}
+
+	metrics.TopicsProcessed.With(prometheus.Labels{
+		metrics.LabelSyncState: topicResource.Status.SynchronizationState,
+		metrics.LabelPool:      topicResource.Spec.Pool,
+	}).Inc()
+
+	fail := func(err error, state string, retry bool) ReconcileResult {
+		aivenError, ok := err.(aiven.Error)
+		if !ok {
+			status.Message = err.Error()
+			status.Errors = []string{
+				err.Error(),
+			}
+		} else {
+			status.Message = aivenError.Message
+			status.Errors = []string{
+				fmt.Sprintf("%s: %s", aivenError.Message, aivenError.MoreInfo),
+			}
+		}
+		status.SynchronizationState = state
+
+		return ReconcileResult{
+			Requeue: retry,
+			Status:  status,
+			Error:   fmt.Errorf("%s: %s", state, err),
+		}
+	}
+
+	hash, err = topicResource.Spec.Hash()
+	if err != nil {
+		return fail(fmt.Errorf("unable to calculate synchronization hash"), kafka_nais_io_v1.EventFailedPrepare, false)
+	}
+
+	if !topicResource.NeedsSynchronization(hash) {
+		logger.Infof("Synchronization already complete")
+		return ReconcileResult{}
+	}
+
+	if !r.projectWhitelisted(topicResource.Spec.Pool) {
+		return fail(fmt.Errorf("pool '%s' cannot be used in this cluster", topicResource.Spec.Pool), kafka_nais_io_v1.EventFailedPrepare, false)
+	}
+
+	synchronizer := NewSynchronizer(r.Aiven, topicResource, logger)
+	result, err := synchronizer.Synchronize(topicResource)
+	if err != nil {
+		return fail(err, kafka_nais_io_v1.EventFailedSynchronization, true)
+	}
+
+	secrets := make([]v1.Secret, len(result.users))
+	for i, user := range result.users {
+		secret, err := Secret(topicResource, r.StoreGenerator, *user, result.brokers, result.registry, result.ca)
+		if err != nil {
+			return fail(err, kafka_nais_io_v1.EventFailedPrepare, false)
+		}
+		secrets[i] = *secret
+	}
+
+	status.SynchronizationTime = time.Now().Format(time.RFC3339)
+	status.SynchronizationState = kafka_nais_io_v1.EventRolloutComplete
+	status.SynchronizationHash = hash
+	status.CredentialsExpiryTime = time.Now().Add(r.CredentialsLifetime).Format(time.RFC3339)
+	status.Message = "Topic configuration synchronized to Kafka pool"
+	status.Errors = nil
+
+	return ReconcileResult{
+		Status:  kafka_nais_io_v1.TopicStatus{},
+		Secrets: secrets,
+	}
+}
+
+func (r *TopicReconciler) produceSecret(logger *log.Entry, secret v1.Secret) error {
+	logger.Infof("Producing secrets")
+	logger = logger.WithFields(log.Fields{
+		// "username":         user.Username,
+		"secret_namespace": secret.Namespace,
+		"secret_name":      secret.Name,
+	})
+
+	plaintext, err := secret.Marshal()
+	if err != nil {
+		return fmt.Errorf("unable to marshal secret: %w", err)
+	}
+
+	ciphertext, err := r.CryptManager.Encrypt(plaintext)
+	if err != nil {
+		return fmt.Errorf("unable to encrypt secret: %w", err)
+	}
+
+	offset, err := r.Producer.Produce(ciphertext)
+	if err != nil {
+		return fmt.Errorf("unable to produce secret on Kafka: %w", err)
+	}
+
+	logger.WithFields(log.Fields{
+		"kafka_offset": offset,
+	}).Infof("Produced secret")
+
+	return nil
+}
+
 // +kubebuilder:rbac:groups=kafka.nais.io,resources=topics,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=kafka.nais.io,resources=topics/status,verbs=get;update;patch
 
 func (r *TopicReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	var topic kafka_nais_io_v1.Topic
+
 	logger := log.NewEntry(r.Logger)
 
 	logger = logger.WithFields(log.Fields{
@@ -72,160 +183,58 @@ func (r *TopicReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	})
 
 	logger.Infof("Processing request")
-
-	ctx := context.Background()
-	hash := ""
-
-	var topicResource kafka_nais_io_v1.Topic
-
-	fail := func(err error, state string, retry bool) (ctrl.Result, error) {
-		aivenError, ok := err.(aiven.Error)
-		if !ok {
-			topicResource.Status.Message = err.Error()
-			topicResource.Status.Errors = []string{
-				err.Error(),
-			}
-		} else {
-			topicResource.Status.Message = aivenError.Message
-			topicResource.Status.Errors = []string{
-				fmt.Sprintf("%s: %s", aivenError.Message, aivenError.MoreInfo),
-			}
-		}
-		topicResource.Status.SynchronizationState = state
-
-		logger.Errorf("%s: %s", state, err)
-
-		if retry {
-			return ctrl.Result{
-				RequeueAfter: r.RequeueInterval,
-			}, nil
-		} else {
-			return ctrl.Result{}, nil
-		}
-	}
-
 	defer func() {
 		logger.Infof("Finished processing request")
 	}()
 
-	// purge other systems if resource was deleted
-	err := r.Get(ctx, req.NamespacedName, &topicResource)
+	ctx := context.Background()
+
+	fail := func(err error, requeue bool) (ctrl.Result, error) {
+		logger.Error(err)
+		cr := &ctrl.Result{}
+		if requeue {
+			cr.RequeueAfter = r.RequeueInterval
+		}
+		return *cr, nil
+	}
+
+	// todo: purge Aiven if resource was deleted and annotation is set?
+	err := r.Get(ctx, req.NamespacedName, &topic)
 	switch {
 	case errors.IsNotFound(err):
-		logger.Errorf("Resource deleted from cluster; noop")
-		return ctrl.Result{}, nil
+		return fail(fmt.Errorf("resource deleted from cluster; noop"), false)
 	case err != nil:
-		logger.Errorf("Unable to retrieve resource from cluster: %s", err)
-		return ctrl.Result{
-			RequeueAfter: r.RequeueInterval,
-		}, nil
+		return fail(fmt.Errorf("unable to retrieve resource from cluster: %s", err), true)
 	}
 
 	logger = logger.WithFields(log.Fields{
-		"team":          topicResource.Labels["team"],
-		"aiven_project": topicResource.Spec.Pool,
-		"aiven_topic":   topicResource.FullName(),
+		"team":          topic.Labels["team"],
+		"aiven_project": topic.Spec.Pool,
+		"aiven_topic":   topic.FullName(),
 	})
 
-	if topicResource.Status == nil {
-		topicResource.Status = &kafka_nais_io_v1.TopicStatus{}
+	// Sync to Aiven; retry if necessary
+	result := r.Process(topic, logger)
+	if result.Error != nil {
+		return fail(err, result.Requeue)
 	}
 
-	hash, err = topicResource.Spec.Hash()
-	if err != nil {
-		logger.Errorf("Unable to calculate synchronization hash")
-		return ctrl.Result{}, err
-	}
-
-	if !topicResource.NeedsSynchronization(hash) {
-		logger.Infof("Synchronization already complete")
-		return ctrl.Result{}, nil
-	}
-
-	// Processing starts here
-	if !r.projectWhitelisted(topicResource.Spec.Pool) {
-		return fail(fmt.Errorf("pool '%s' cannot be used in this cluster", topicResource.Spec.Pool), kafka_nais_io_v1.EventFailedPrepare, false)
-	}
-
-	// Update Topic resource with status event
-	defer func() {
-		topicResource.Status.SynchronizationTime = time.Now().Format(time.RFC3339)
-		err := r.Update(ctx, &topicResource)
+	// Produce secrets; retry always
+	for _, secret := range result.Secrets {
+		err = r.produceSecret(logger, secret)
 		if err != nil {
-			logger.Errorf("failed writing topic status: %s", err)
+			return fail(err, true)
 		}
-		metrics.TopicsProcessed.With(prometheus.Labels{
-			metrics.LabelSyncState: topicResource.Status.SynchronizationState,
-			metrics.LabelPool:      topicResource.Spec.Pool,
-		}).Inc()
-	}()
-
-	// prepare and commit
-	tx := &transaction{
-		ctx:    ctx,
-		req:    req,
-		topic:  topicResource,
-		logger: logger,
 	}
 
-	err = r.commit(*tx)
-
+	// Write topic status; retry always
+	topic.Status = &result.Status
+	err = r.Update(ctx, &topic)
 	if err != nil {
-		return fail(err, kafka_nais_io_v1.EventFailedSynchronization, true)
+		return fail(err, true)
 	}
-
-	credentialsThreshold := time.Now().Add(r.CredentialsLifetime)
-
-	topicResource.Status.SynchronizationState = kafka_nais_io_v1.EventRolloutComplete
-	topicResource.Status.SynchronizationHash = hash
-	topicResource.Status.CredentialsExpiryTime = credentialsThreshold.Format(time.RFC3339)
-	topicResource.Status.Message = "Topic configuration synchronized to Kafka pool"
-	topicResource.Status.Errors = nil
 
 	return ctrl.Result{}, nil
-}
-
-func (r *TopicReconciler) commit(tx transaction) error {
-	synchronizer := NewSynchronizer(r.Aiven, tx.topic, tx.logger)
-	result, err := synchronizer.Synchronize(tx.topic)
-	if err != nil {
-		return err
-	}
-
-	tx.logger.Infof("Producing secrets")
-	for _, user := range result.users {
-		secret, err := Secret(tx.topic, r.StoreGenerator, *user, result.brokers, result.registry, result.ca)
-		if err != nil {
-			return err
-		}
-
-		tx.logger = tx.logger.WithFields(log.Fields{
-			"username":         user.Username,
-			"secret_namespace": secret.Namespace,
-			"secret_name":      secret.Name,
-		})
-
-		plaintext, err := secret.Marshal()
-		if err != nil {
-			return fmt.Errorf("unable to marshal secret: %w", err)
-		}
-
-		ciphertext, err := r.CryptManager.Encrypt(plaintext)
-		if err != nil {
-			return fmt.Errorf("unable to encrypt secret: %w", err)
-		}
-
-		offset, err := r.Producer.Produce(ciphertext)
-		if err != nil {
-			return fmt.Errorf("unable to produce secret on Kafka: %w", err)
-		}
-
-		tx.logger.WithFields(log.Fields{
-			"kafka_offset": offset,
-		}).Infof("Produced secret")
-	}
-
-	return nil
 }
 
 func (r *TopicReconciler) SetupWithManager(mgr ctrl.Manager) error {
