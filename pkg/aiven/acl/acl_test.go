b5d6e17f54f46b56cce9b4447bcff27a8cad168c
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-06-01 16:10:07 +0200

    Create ACLs with wildcardsuffixed usernames

--- pkg/aiven/acl/acl_test.go
+++ pkg/aiven/acl/acl_test.go
@@ -1,80 +1,96 @@
 package acl_test
 
 import (
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/suite"
+	"gotest.tools/assert"
 	"testing"
 
 	"github.com/aiven/aiven-go-client"
-	"github.com/nais/liberator/pkg/apis/kafka.nais.io/v1"
 	"github.com/nais/kafkarator/pkg/aiven/acl"
-	"github.com/stretchr/testify/assert"
+	"github.com/nais/liberator/pkg/apis/kafka.nais.io/v1"
 )
 
-func TestACLFilter(t *testing.T) {
-	existingACLs := []*aiven.KafkaACL{
-		{
+type ACLFilterTestSuite struct {
+	suite.Suite
+
+	existingACLs []*aiven.KafkaACL
+	aclSpecs     []kafka_nais_io_v1.TopicACL
+	shouldAdd    []kafka_nais_io_v1.TopicACL
+	shouldRemove []*aiven.KafkaACL
+}
+
+func (suite *ACLFilterTestSuite) SetupSuite() {
+	suite.existingACLs = []*aiven.KafkaACL{
+		{ // Delete because of username
 			ID:         "abc",
 			Permission: "read",
 			Topic:      "topic",
 			Username:   "user.app-f1fbd6bd",
 		},
-		{
+		{ // Delete because of wrong permission
 			ID:         "abcde",
 			Permission: "write",
 			Topic:      "topic",
-			Username:   "user.app-f1fbd6bd",
+			Username:   "user.app*",
 		},
-		{
+		{ // Delete because of username and permission
 			ID:         "123",
 			Permission: "read",
 			Topic:      "topic",
 			Username:   "user2.app-4ca551f9",
 		},
-		{
+		{ // Keep
 			ID:         "abcdef",
 			Permission: "write",
 			Topic:      "topic",
-			Username:   "user2.app-4ca551f9",
-		},
-		{
-			ID:         "abc",
-			Permission: "read",
-			Topic:      "not_our_topic",
-			Username:   "user.app-f1fbd6bd",
+			Username:   "user2.app*",
 		},
 	}
 
-	aclSpecs := []kafka_nais_io_v1.TopicACL{
+	suite.aclSpecs = []kafka_nais_io_v1.TopicACL{
+		{ // Added because existing uses wrong username
+			Access:      "read",
+			Team:        "user",
+			Application: "app",
+		},
+		{ // Already exists
+			Access:      "write",
+			Team:        "user2",
+			Application: "app",
+		},
+		{ // Added because of new user
+			Access:      "readwrite",
+			Team:        "user3",
+			Application: "app",
+		},
+	}
+
+	suite.shouldAdd = []kafka_nais_io_v1.TopicACL{
 		{
 			Access:      "read",
 			Team:        "user",
 			Application: "app",
 		},
 		{
-			Access:      "write",
-			Team:        "user2",
-			Application: "app",
-		},
-		{
 			Access:      "readwrite",
 			Team:        "user3",
 			Application: "app",
 		},
 	}
 
-	shouldAdd := []kafka_nais_io_v1.TopicACL{
+	suite.shouldRemove = []*aiven.KafkaACL{
 		{
-			Access:      "readwrite",
-			Team:        "user3",
-			Application: "app",
+			ID:         "abc",
+			Permission: "read",
+			Topic:      "topic",
+			Username:   "user.app-f1fbd6bd",
 		},
-	}
-
-	shouldRemove := []*aiven.KafkaACL{
 		{
 			ID:         "abcde",
 			Permission: "write",
 			Topic:      "topic",
-			Username:   "user.app-f1fbd6bd",
+			Username:   "user.app*",
 		},
 		{
 			ID:         "123",
@@ -83,11 +99,25 @@ func TestACLFilter(t *testing.T) {
 			Username:   "user2.app-4ca551f9",
 		},
 	}
+}
 
-	added := acl.NewACLs(existingACLs, aclSpecs)
-	removed := acl.DeleteACLs(existingACLs, aclSpecs)
+func (suite *ACLFilterTestSuite) TestNewACLs() {
+	added := acl.NewACLs(suite.existingACLs, suite.aclSpecs)
 
-	assert.Equal(t, shouldAdd, added)
+	assert.DeepEqual(suite.T(), suite.shouldAdd, added, cmpopts.SortSlices(func(a, b kafka_nais_io_v1.TopicACL) bool {
+		return a.Application < b.Application
+	}))
+}
 
-	assert.Equal(t, shouldRemove, removed)
+func (suite *ACLFilterTestSuite) TestDeleteACLs() {
+	removed := acl.DeleteACLs(suite.existingACLs, suite.aclSpecs)
+
+	assert.DeepEqual(suite.T(), suite.shouldRemove, removed, cmpopts.SortSlices(func(a, b *aiven.KafkaACL) bool {
+		return a.ID < b.ID
+	}))
+}
+
+func TestACLFilter(t *testing.T) {
+	testSuite := new(ACLFilterTestSuite)
+	suite.Run(t, testSuite)
 }
