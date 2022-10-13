b4bfbd0ad5aab965a196ae6cbfde861a569c0b8d
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-10-13 14:24:42 +0200

    Added script to delete users with no related secret in any known cluster
    Users with no related secret are either not created by our operators,
    or no longer accessible by any application.
    If the user matches one of the conventions used for generated users,
    it can be assumed to belong to the last group and is eligible for deletion.

---
+++ scripts/user_cleaner.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import os
+import re
+import subprocess
+from dataclasses import dataclass
+
+import requests
+
+OPERATOR_PATTERNS = (
+    re.compile(r"[^_]+_[^_]+_[^_]+_.+"),
+    re.compile(r".*\..*"),
+)
+
+
+class AivenAuth(requests.auth.AuthBase):
+    def __init__(self, token=None):
+        if token is None:
+            token = os.getenv("AIVEN_TOKEN")
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = f"Bearer {self.token}"
+        return r
+
+
+@dataclass(frozen=True)
+class Secret:
+    username: str
+    name: str
+    namespace: str
+    context: str
+
+
+@dataclass(frozen=True)
+class User:
+    username: str
+    type: str
+
+
+@dataclass
+class Service:
+    users: set[User]
+
+
+class AivenKafka(object):
+    base = "https://api.aiven.io/v1/project"
+
+    def __init__(self, project, service=None, dry_run=False):
+        self.project = project
+        if service is None:
+            service = project + "-kafka"
+        self.service = service
+        self.dry_run = dry_run
+        self.session = requests.Session()
+        self.session.auth = AivenAuth()
+        self.base_url = f"{self.base}/{self.project}/service/{self.service}"
+
+    def get_service(self):
+        resp = self.session.get(self.base_url)
+        resp.raise_for_status()
+        data = resp.json()
+        # users = {User(u["username"], u["type"]) for u in data["service"]["users"]
+        #          if any(p.match(u["username"]) for p in OPERATOR_PATTERNS)}
+        return Service(set(self.extract_users(data)))
+
+    @staticmethod
+    def extract_users(data):
+        for u in data["service"]["users"]:
+            if any(p.match(u["username"]) for p in OPERATOR_PATTERNS):
+                yield User(u["username"], u["type"])
+            else:
+                print(f"Ignoring {u['username']}, since it did not match any pattern")
+
+    def delete_users(self, users_to_delete: set[str]):
+        for username in users_to_delete:
+            if not self.dry_run:
+                print(f"Deleting {username}")
+                resp = self.session.delete(f"{self.base_url}/user/{username}")
+                resp.raise_for_status()
+            else:
+                print(f"Would have deleted {username}")
+
+
+def get_secrets_in(context):
+    cmd = [
+        "kubectl",
+        "get", "secret",
+        "--context", context,
+        "--all-namespaces",
+        "--output", "json",
+        "--selector", "type=aivenator.aiven.nais.io"
+    ]
+    print(f"Executing {' '.join(cmd)}")
+    output = subprocess.check_output(cmd)
+    data = json.loads(output)
+    for item in data["items"]:
+        metadata = item["metadata"]
+        name = metadata["name"]
+        namespace = metadata["namespace"]
+        annotations = metadata.get("annotations", {})
+        username = annotations.get("kafka.aiven.nais.io/serviceUser")
+        if username:
+            yield Secret(username, name, namespace, context)
+
+
+def get_secrets(contexts) -> set[Secret]:
+    secrets = set()
+    for context in contexts:
+        secrets.update(set(get_secrets_in(context)))
+    return secrets
+
+
+def find_unused_users(secrets: set[Secret], users: set[User]) -> set[str]:
+    secret_usernamess = {s.username for s in secrets}
+    user_usernames = {u.username for u in users}
+    return user_usernames - secret_usernamess
+
+
+def main(env, dry_run):
+    project = f"nav-{env}"
+    contexts = {f"{env}-{kind}" for kind in ("fss", "gcp")}
+
+    aiven = AivenKafka(project, dry_run=dry_run)
+
+    service = aiven.get_service()
+    print(f"Aiven knows {len(service.users)} users")
+    secrets = get_secrets(contexts)
+    print(f"Found {len(secrets)} secrets in all clusters")
+    users_to_delete = set(find_unused_users(secrets, service.users))
+    print(f"Found {len(users_to_delete)} users with no associated secret")
+    aiven.delete_users(users_to_delete)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--dry-run", action="store_true", help="Make no actual changes")
+    parser.add_argument("env", action="store", help="Environment to process")
+    options = parser.parse_args()
+    main(options.env, options.dry_run)
