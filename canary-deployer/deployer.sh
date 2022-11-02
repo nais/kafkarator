81d10869dea6e8dd2ff3dd747ffdd98532545741
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-11-02 20:11:44 +0100

    Prepare canary deployer for NaaS usage

--- canary-deployer/deployer.sh
+++ canary-deployer/deployer.sh
@@ -3,21 +3,26 @@
 VARS=$(mktemp)
 export VARS
 
-for CLUSTER in $CLUSTERS; do
-  if [ "$CLUSTER" = "prod-fss" -o "$CLUSTER" = "prod-gcp" -o "$CLUSTER" = "prod-sbs" ]; then
+for CLUSTER in ${CLUSTERS}; do
+  if [ "${CLUSTER}" = "prod-fss" ] || [ "${CLUSTER}" = "prod-gcp" ]; then
     POOL=nav-prod
-  else
+  elif [ "${CLUSTER}" = "dev-fss" ] || [ "${CLUSTER}" = "dev-gcp" ]; then
     POOL=nav-dev
+  else
+    POOL="${TENANT:-nav}-${CLUSTER}"
   fi
+
   export CLUSTER
   {
     echo "---"
-    echo "image: $IMAGE"
     echo "now: $(date +%s)000000000"
-    echo "groupid: $CLUSTER"
-    echo "pool: $POOL"
-  } > "$VARS"
-  cat "$VARS"
-  echo "Deploying to $CLUSTER..."
+    echo "image: ${IMAGE}"
+    echo "groupid: ${CLUSTER}"
+    echo "pool: ${POOL}"
+    echo "team: ${TEAM:-aura}"
+    echo "canary_kafka_topic: ${TOPIC:-aura.kafkarator-canary-${CLUSTER}}"
+  } > "${VARS}"
+  cat "${VARS}"
+  echo "Deploying to ${CLUSTER}..."
   /app/deploy --wait=false
 done
