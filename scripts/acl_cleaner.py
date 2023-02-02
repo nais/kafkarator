1aa099dc01bbdfc6403fba5c8c84952386daacb9
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-02-02 09:56:11 +0100

    scripts: Refactor and extract common parts

--- scripts/acl_cleaner.py
+++ scripts/acl_cleaner.py
@@ -1,89 +1,10 @@
 #!/usr/bin/env python3
 import argparse
-import os
-from dataclasses import dataclass
 from fnmatch import fnmatch
 
-import requests
+from alive_progress import alive_it
 
-
-class AivenAuth(requests.auth.AuthBase):
-    def __init__(self, token=None):
-        if token is None:
-            token = os.getenv("AIVEN_TOKEN")
-        self.token = token
-
-    def __call__(self, r):
-        r.headers["authorization"] = f"Bearer {self.token}"
-        return r
-
-
-@dataclass(frozen=True)
-class Acl:
-    id: str
-    permission: str
-    topic: str
-    username: str
-
-
-@dataclass
-class Service:
-    acls: set[Acl]
-    topics: set[str]
-    users: set[str]
-
-
-class AivenKafka(object):
-    base = "https://api.aiven.io/v1/project"
-
-    def __init__(self, project, service=None, dry_run=False):
-        self.project = project
-        if service is None:
-            service = project + "-kafka"
-        self.service = service
-        self.dry_run = dry_run
-        self.session = requests.Session()
-        self.session.auth = AivenAuth()
-        self.base_url = f"{self.base}/{self.project}/service/{self.service}"
-
-    def get_topics(self):
-        resp = self.session.get(f"{self.base_url}/topic")
-        resp.raise_for_status()
-        data = resp.json()
-        return [t["topic_name"] for t in data["topics"]]
-
-    def get_service(self):
-        resp = self.session.get(self.base_url)
-        resp.raise_for_status()
-        data = resp.json()
-        acls = {Acl(**a) for a in data["service"]["acl"]}
-        topics = {t["topic_name"] for t in data["service"]["topics"]}
-        users = {u["username"] for u in data["service"]["users"]}
-        return Service(acls, topics, users)
-
-    def get_acls(self):
-        resp = self.session.get(f"{self.base_url}/acl")
-        resp.raise_for_status()
-        data = resp.json()
-        return {Acl(**a) for a in data["acl"]}
-
-    def delete_acls(self, acls_to_delete):
-        for acl in acls_to_delete:
-            if not self.dry_run:
-                print(f"Deleting {acl}")
-                resp = self.session.delete(f"{self.base_url}/acl/{acl.id}")
-                resp.raise_for_status()
-            else:
-                print(f"Would have deleted {acl}")
-
-    def delete_users(self, users_to_delete):
-        for username in users_to_delete:
-            if not self.dry_run:
-                print(f"Deleting {username}")
-                resp = self.session.delete(f"{self.base_url}/user/{username}")
-                resp.raise_for_status()
-            else:
-                print(f"Would have deleted {username}")
+from common import AivenKafka, User
 
 
 def find_unused_acls(topics, acls):
@@ -95,13 +16,13 @@ def find_unused_acls(topics, acls):
         yield acl
 
 
-def find_unused_users(acls, users):
+def find_unused_users(acls, users: set[User]):
     acl_patterns = {acl.username for acl in acls}
-    for username in users:
-        if username == "avnadmin":
+    for user in alive_it(users):
+        if user.username == "avnadmin":
             continue
-        if not any(fnmatch(username, pattern) for pattern in acl_patterns):
-            yield username
+        if not any(fnmatch(user.username, pattern) for pattern in acl_patterns):
+            yield user.username
 
 
 def main(project, dry_run):
---
+++ scripts/common.py
@@ -0,0 +1,104 @@
+import os
+import re
+from dataclasses import dataclass
+
+import requests
+
+OPERATOR_PATTERNS = (
+    re.compile(r"[^_]+_[^_]+_[^_]+_.+"),
+    re.compile(r".*\..*"),
+)
+
+
+class AivenAuth(requests.auth.AuthBase):
+    def __init__(self, token=None):
+        if token is None:
+            token = os.getenv("AIVEN_TOKEN")
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = f"Bearer {self.token}"
+        return r
+
+
+@dataclass(frozen=True)
+class Acl:
+    id: str
+    permission: str
+    topic: str
+    username: str
+
+
+@dataclass(frozen=True)
+class User:
+    username: str
+    type: str
+
+
+@dataclass
+class Service:
+    acls: set[Acl]
+    topics: set[str]
+    users: set[User]
+
+
+class AivenKafka(object):
+    base = "https://api.aiven.io/v1/project"
+
+    def __init__(self, project, service=None, dry_run=False):
+        self.project = project
+        if service is None:
+            service = project + "-kafka"
+        self.service = service
+        self.dry_run = dry_run
+        self.session = requests.Session()
+        self.session.auth = AivenAuth()
+        self.base_url = f"{self.base}/{self.project}/service/{self.service}"
+
+    def get_topics(self):
+        resp = self.session.get(f"{self.base_url}/topic")
+        resp.raise_for_status()
+        data = resp.json()
+        return [t["topic_name"] for t in data["topics"]]
+
+    def get_service(self, team=None):
+        resp = self.session.get(self.base_url)
+        resp.raise_for_status()
+        data = resp.json()
+        acls = {Acl(**a) for a in data["service"]["acl"]}
+        topics = {t["topic_name"] for t in data["service"]["topics"]}
+        users = set(self.extract_users(data, team))
+        return Service(acls, topics, users)
+
+    def get_acls(self):
+        resp = self.session.get(f"{self.base_url}/acl")
+        resp.raise_for_status()
+        data = resp.json()
+        return {Acl(**a) for a in data["acl"]}
+
+    def delete_acls(self, acls_to_delete):
+        for acl in acls_to_delete:
+            if not self.dry_run:
+                print(f"Deleting {acl}")
+                resp = self.session.delete(f"{self.base_url}/acl/{acl.id}")
+                resp.raise_for_status()
+            else:
+                print(f"Would have deleted {acl}")
+
+    def delete_users(self, users_to_delete):
+        for username in users_to_delete:
+            if not self.dry_run:
+                print(f"Deleting {username}")
+                resp = self.session.delete(f"{self.base_url}/user/{username}")
+                resp.raise_for_status()
+            else:
+                print(f"Would have deleted {username}")
+
+    @staticmethod
+    def extract_users(data, team):
+        for u in data["service"]["users"]:
+            if any(p.match(u["username"]) for p in OPERATOR_PATTERNS):
+                if not team or u["username"].startswith(team):
+                    yield User(u["username"], u["type"])
+            else:
+                print(f"Ignoring {u['username']}, since it did not match any pattern")
