6da7b8dc32b1c8928282d864cb66eea8d789b12e
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-12-19 10:41:53 +0100

    Better sync_all_topics.sh script

--- scripts/sync_all_topics.sh
+++ scripts/sync_all_topics.sh
@@ -2,14 +2,20 @@
 
 # Forces sync of all topics in all namespaces by removing sync hash from status
 
-for ns in $(kubectl get ns -ocustom-columns=NAME:.metadata.name --no-headers); do
+#kubectl get ns -ocustom-columns=NAME:.metadata.name --no-headers > namespaces
+
+while IFS="" read -r ns; do
+
   echo "Resyncing topics in ${ns}"
   for topic in $(kubectl get topic "--namespace=${ns}" -ocustom-columns=NAME:.metadata.name --no-headers); do
     kubectl patch topic "--namespace=${ns}" "${topic}" --type=merge --patch '{"status":{"synchronizationHash":"reset"}}'
+    sleep 1
+  done
+
   echo "Resyncing streams in ${ns}"
   for stream in $(kubectl get stream "--namespace=${ns}" -ocustom-columns=NAME:.metadata.name --no-headers); do
     kubectl patch stream "--namespace=${ns}" "${stream}" --type=merge --patch '{"status":{"synchronizationHash":"reset"}}'
-
+    sleep 1
   done
 
-done
+done < namespaces
